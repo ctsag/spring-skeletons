cc35ccf31daf698311e625848a0041cca5555cd2
Christos Tsagkournis <Christos.Tsagkournis@sgdigital.com>
2020-10-23 21:31:52 +0300

    Adding a basic RESTful service

---
+++ restful-service/src/main/java/gr/nothingness/springskeletons/restfulservice/Greeting.java
@@ -0,0 +1,12 @@
+package gr.nothingness.springskeletons.restfulservice;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Greeting {
+
+  @Getter private final long id;
+  @Getter private final String content;
+
+}
---
+++ restful-service/src/main/java/gr/nothingness/springskeletons/restfulservice/RESTfulServiceApplication.java
@@ -0,0 +1,13 @@
+package gr.nothingness.springskeletons.restfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTfulServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RESTfulServiceApplication.class, args);
+  }
+
+}
