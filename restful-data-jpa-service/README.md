9f221295fa7a36703ce6d44c76dce0e624ccfbfb
Christos Tsagkournis <Christos.Tsagkournis@sgdigital.com>
2020-10-27 22:36:56 +0200

    Adding source code for a RESTful Data JPA service skeleton app

---
+++ restful-data-jpa-service/README.md
@@ -0,0 +1,8 @@
+# Contents
+This skeleton application involves the following areas :
+* Data JPA
+* REST Repositories
+* Spring HATEOAS
+
+# References
+* [Accessing JPA Data with REST](https://spring.io/guides/gs/accessing-data-rest/)
---
+++ restful-data-jpa-service/src/test/java/gr/nothingness/springskeletons/restfuldatajpaservice/ResTfulDataJpaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.nothingness.springskeletons.restfuldatajpaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulDataJpaServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
