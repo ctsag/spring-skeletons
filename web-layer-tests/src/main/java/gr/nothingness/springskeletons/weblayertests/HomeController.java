8d3bffd44b21b2a805459acf9a11c52c086a8779
Christos Tsagkournis <Christos.Tsagkournis@sgdigital.com>
2020-11-07 13:49:22 +0200

    Adding source code for web layer tests

---
+++ web-layer-tests/src/main/java/gr/nothingness/springskeletons/weblayertests/HomeController.java
@@ -0,0 +1,21 @@
+package gr.nothingness.springskeletons.weblayertests;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+  private final GreetingService service;
+
+  public HomeController(GreetingService service) {
+    this.service = service;
+  }
+
+  @RequestMapping("/")
+  public @ResponseBody String greeting() {
+    return service.greet();
+  }
+
+}
